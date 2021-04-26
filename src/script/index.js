56a42033515f4f3f7ad73de8f82abff36027b8a8
hamza <darejhamza@gmail.com>
2021-04-26 12:43:51 +0200

    todolist

--- src/script/index.js
+++ src/script/index.js
@@ -1,11 +1,16 @@
 const addIt= () =>{
  let userDate = document.querySelector("#userDate").value;  
  let result=document.querySelector(".result");
+ let checkbox =document.createElement("input");
+ let checkboxes=document.querySelector(".checkboxes");
+ checkbox.type="checkbox"
  if (userDate!=""){
      let newLi =document.createElement("li");
      let text = document.createTextNode(userDate);
      newLi.appendChild(text);
      result.appendChild(newLi);
+     checkboxes.appendChild(checkbox);
+     
      document.querySelector("#userDate").value="";
  }
  else {
